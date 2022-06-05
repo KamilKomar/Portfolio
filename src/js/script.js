4b33b6c8c3dbd30b55e2288aca07c40cb76550fe
Kamil <kamil.56@wp.pl>
2022-06-05 16:12:17 +0200

    about me section mobile and desktop created

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,11 @@
 const navMobileMenuBtn = document.querySelector('.nav__mobile-menu')
 const navMobileCloseBtn = document.querySelector('.nav__mobile-close')
-console.log(navMobileCloseBtn)
+
+
 const nav = document.querySelector('.nav')
 
-let controller = new ScrollMagic.Controller();
-let timeline = new TimelineMax();
+// let controller = new ScrollMagic.Controller();
+// let timeline = new TimelineMax();
 
 
 navMobileMenuBtn.addEventListener('click', () => {
@@ -17,19 +18,19 @@ navMobileCloseBtn.addEventListener('click', () => {
 })
 
 
-timeline
-    .to(".mountain1", 3, { y: -200 })
-    .to(".mountain2", 3, { y: -500 }, "-=3")
-    .fromTo(".bg", 3, { y: -50 }, { y: 0, duration: 3 }, "-=3")
-    .to(".welcome", 3, { top: "0%" }, "-=3");
-    // .fromTo(".welcome", { opacity: 0 }, { opacity: 1, duration: 3 });
-    // .fromTo(".welcome__text", { opacity: 0 }, { opacity: 1 });
+// timeline
+//     .to(".mountain1", 3, { y: -200 })
+//     .to(".mountain2", 3, { y: -500 }, "-=3")
+//     .fromTo(".bg", 3, { y: -50 }, { y: 0, duration: 3 }, "-=3")
+//     .to(".main", 3, { top: "-80%" }, "-=3");
+//     // .fromTo(".welcome", { opacity: 0 }, { opacity: 1, duration: 3 });
+//     // .fromTo(".welcome__text", { opacity: 0 }, { opacity: 1 });
 
-let scene = new ScrollMagic.Scene({
-    triggerElement: "header",
-    duration: "150%",
-    triggerHook: 0,
-})
-    .setTween(timeline)
-    .setPin("welcome")
-    .addTo(controller);
+// let scene = new ScrollMagic.Scene({
+//     triggerElement: "header",
+//     duration: "150%",
+//     triggerHook: 0,
+// })
+//     .setTween(timeline)
+//     .setPin("main")
+//     .addTo(controller);
